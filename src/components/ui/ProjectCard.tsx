c5077333fa31d7d4bfdc4ec5bf96bb84f6969c6b
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-12 23:48:36 +1100

    finished creating ProjectCard component

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { ProjectCard as ProjectCardProps } from "@/projects";
 import {
   Card,
   CardContent,
@@ -8,34 +8,58 @@ import {
   CardTitle,
 } from "./card";
 import { Button } from "./button";
-
-interface ProjectCardProps {
-  title: string;
-  description: string;
-  imgSrc: string;
-  liveUrl?: string;
-  githubUrl?: string;
-  tags?: string;
-}
+import { Badge } from "./badge";
+import { ExternalLink, Github } from "lucide-react";
+import DisplayTags from "../features/DisplayTags";
 
 const ProjectCard = (props: ProjectCardProps) => {
   return (
-    <Card className="max-w-80">
-      <div className="card-img-container relative overflow-hidden">
+    <Card className="flex flex-col pt-0 overflow-hidden border border-border hover:border-primary/50 hover:shadow-md">
+      <div className="card-img-container relative overflow-hidden h-80">
         <img
           src={props.imgSrc}
           alt="project_photo"
-          className="object-cover w-full h-full"
+          className="object-cover w-full"
         />
       </div>
-      <CardHeader>
+      <CardHeader className="flex-grow">
         <CardTitle>{props.title}</CardTitle>
         <CardDescription>{props.description}</CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
-      <CardFooter className="flex">
-        {props.liveUrl && <Button>Live Demo</Button>}
-        <Button>More Details</Button>
+      <CardContent>
+        {/* <div className="flex space-x-1">
+          {props.tags.map((tag) => {
+            return <Badge variant={"secondary"}>{tag}</Badge>;
+          })}
+        </div> */}
+        <DisplayTags tags={props.tags} />
+      </CardContent>
+      <CardFooter className="flex space-x-1">
+        <Button className="hover:cursor-pointer">More Details</Button>
+        {props.liveUrl && (
+          <Button variant="outline" className="hover:cursor-pointer">
+            <ExternalLink />
+            <a
+              href="https://google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
+          </Button>
+        )}
+        {props.githubUrl && (
+          <Button variant="outline" className="hover:cursor-pointer">
+            <Github />
+            <a
+              href="https://google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+          </Button>
+        )}
       </CardFooter>
     </Card>
   );
