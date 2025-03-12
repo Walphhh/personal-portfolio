34d622b18a2d54fd039465b206ae2eed451b264a
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-12 23:52:28 +1100

    moved hero contents to hero section

---
+++ src/components/features/Hero.tsx
@@ -0,0 +1,57 @@
+import Hello from "./Hello";
+import Fullscreen from "../ui/Fullscreen";
+import React from "react";
+import { Separator } from "../ui/separator";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { Github } from "lucide-react";
+
+const Hero = () => {
+  return (
+    <>
+      <Fullscreen className="bg-amber-50 flex items-center justify-center space-x-10">
+        <div className="m-2 space-y-5 mr-10">
+          <div className="space-y-10">
+            <div>
+              <h1 className="text-2xl flex">
+                <div className="text-2xl min-w-40">
+                  <Hello />
+                </div>
+                I'm Ralph
+              </h1>
+              <h1 className="text-6xl font-semibold">
+                I'm a full-stack dev :3
+              </h1>
+            </div>
+            <div className="flex items-center space-x-5">
+              <Button className="hover:cursor-pointer">Download CV</Button>
+              <a href="">
+                <img
+                  src="src/assets/github.svg"
+                  alt="github_icon"
+                  className="size-12 hover:opacity-80"
+                />
+              </a>
+              <a href="">
+                <img
+                  src="src/assets/linkedin.svg"
+                  alt="linkedin_icon"
+                  className="size-15 hover:opacity-80"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <Separator orientation="vertical" className="min-h-50 min-w-1" />
+
+        <Avatar className="w-50 h-50">
+          <AvatarImage src="src\assets\sillycat.jpg" />
+          <AvatarFallback>CAT</AvatarFallback>
+        </Avatar>
+      </Fullscreen>
+    </>
+  );
+};
+
+export default Hero;
