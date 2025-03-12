18bda4165d3a42b852394d14cd97e62c796db0be
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-13 01:36:44 +1100

    moved files

--- src/components/information/experience.ts
+++ src/components/information/experience.ts
@@ -11,6 +11,7 @@ export interface ExperienceCard {
   experienceType: experienceType;
   position: string;
   company: string;
+  companyLogo: string;
   location: string;
   description: string;
   duration: string;
@@ -23,6 +24,7 @@ export const experience: ExperienceCard[] = [
     experienceType: "work",
     position: "Student Belong Activator",
     company: "Macquarie University",
+    companyLogo: "src/assets/mqu_logo.png",
     location: "Sydney, Australia ",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -33,6 +35,7 @@ export const experience: ExperienceCard[] = [
     experienceType: "work",
     position: "Student Ambassador",
     company: "Faculty of Science and Engineering MQ",
+    companyLogo: "src/assets/mqu_logo.png",
     location: "Sydney, Australia ",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -43,6 +46,7 @@ export const experience: ExperienceCard[] = [
     experienceType: "work",
     position: "Secretary",
     company: "Filipino Students Society MQ",
+    companyLogo: "src/assets/mqu_logo.png",
     location: "Sydney, Australia ",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -59,7 +63,7 @@ export const experience: ExperienceCard[] = [
         position: "General Officer",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        duration: "2024 - Present",
+        duration: "2023 - 2024",
       },
     ],
   },
--- src/components/information/projects.ts
+++ src/components/information/projects.ts
@@ -5,7 +5,8 @@ export type TagTypes =
   | "Node.js"
   | "Bootstrap"
   | "ChakraUI"
-  | "Express";
+  | "Express"
+  | "Typescript";
 
 export interface ProjectCard {
   title: string;
@@ -19,31 +20,37 @@ export const projects: ProjectCard[] = [
   {
     title: "Blogging Website",
     description:
-      "A full-stack blogging website built with React + ChakraUI for the frontend and Node.js + Express for the backend. Features jwt for user authentication",
-    imgSrc: "src/assets/sillycat.jpg",
+      "A full-stack blogging website built with React+ChakraUI for the frontend and Node.js+Express for the backend. Deployed to Vercel for the serverless API and Github Pages for the Client",
+    imgSrc: "src/assets/blogsite_snapshot.png",
     tags: ["React", "ChakraUI", "Node.js", "Express"],
-    liveUrl: "https://link/to/project",
-    githubUrl: "https://link/to/github",
+    liveUrl: "https://walphhh.github.io/personal-blog/",
+    githubUrl: "https://github.com/Walphhh/personal-blog",
   },
   {
     title: "Genshin Character viewer",
     description:
       "A viewer for Genshin Impact characters released until mid 2024.",
-    imgSrc: "src/assets/sillycat.jpg",
+    imgSrc: "src/assets/genshinviewer_snapshot.png",
     tags: ["React", "Bootstrap"],
+    liveUrl: "https://walphhh.github.io/Genshin-Encylopedia/",
+    githubUrl: "https://github.com/Walphhh/Genshin-Encylopedia",
+  },
+  {
+    title: "Portfolio Website",
+    description: "A website showcasing my portfolio",
+    imgSrc: "src/assets/portfolio_snapshot.png",
+    tags: ["React", "Typescript"],
+    githubUrl: "",
   },
   {
     title: "Christmas Card",
     description: "Sharing the holiday spirit with an online card :3",
-    imgSrc: "src/assets/sillycat.jpg",
+    imgSrc: "src/assets/christmas_card_snapshot.png",
     tags: ["React"],
+    liveUrl: "https://walphhh.github.io/for-friends/",
+    githubUrl: "https://github.com/Walphhh/for-friends",
   },
-  {
-    title: "some title for ANOTHER project",
-    description: "some desription about ANOTHER project",
-    imgSrc: "src/assets/sillycat.jpg",
-    tags: ["Placeholder"],
-  },
+
   {
     title: "some title for ANOTHER project",
     description: "some desription about ANOTHER project",
