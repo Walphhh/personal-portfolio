cd1aaf2496093e51bbe9a9633ed05e306bd2dcac
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-13 01:38:28 +1100

    syntax changes

--- src/components/features/Projects.tsx
+++ src/components/features/Projects.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Fullscreen from "../ui/Fullscreen";
 import ProjectCard from "../ui/ProjectCard";
-import { projects } from "@/projects";
+import { projects } from "@/components/information/projects";
 
 const Projects = () => {
-  const projectsList = projects;
-
   return (
     <Fullscreen className="p-8 space-y-10 px-8 lg:px-48 ">
       <div className="flex justify-center">
         <h1 className="text-4xl font-bold">My Projects</h1>
       </div>
       <div className="projects-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projectsList.map((project) => {
+        {projects.map((project) => {
           return <ProjectCard {...project} />;
         })}
       </div>
