cd6dce0e19bb3cebffb99de626bcd7c7ed858aab
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-12 23:51:28 +1100

    homepage section refactoring

--- src/components/pages/Homepage.tsx
+++ src/components/pages/Homepage.tsx
@@ -1,32 +1,13 @@
-import Hello from "../features/Hello";
-import Fullscreen from "../ui/Fullscreen";
-import React from "react";
-import { Separator } from "../ui/separator";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import Experience from "../features/Experience";
+import Hero from "../features/Hero";
+import Projects from "../features/Projects";
 
 const Homepage = () => {
   return (
     <>
-      <Fullscreen className="bg-blue-600 flex items-center justify-center space-x-10">
-        <div className="m-2 space-y-5 mr-10">
-          <div className="">
-            <h1 className="text-2xl flex">
-              <div className="text-2xl min-w-40">
-                <Hello />
-              </div>{" "}
-              I'm Ralph
-            </h1>
-            <h1 className="text-6xl">I'm a full-stack dev :3</h1>
-          </div>
-        </div>
-
-        <Separator orientation="vertical" className="min-h-40 min-w-0.5" />
-
-        <Avatar className="w-50 h-50">
-          <AvatarImage src="src\assets\sillycat.jpg" />
-          <AvatarFallback>CAT</AvatarFallback>
-        </Avatar>
-      </Fullscreen>
+      <Hero />
+      <Experience />
+      <Projects />
     </>
   );
 };
