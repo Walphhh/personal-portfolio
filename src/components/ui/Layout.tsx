659f688c7f06c782ad690b0e76299e62273c534e
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-14 18:35:11 +1100

    added disappearing navbar

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,10 +1,39 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../layout/Navbar";
+import { useRef, useState, useEffect } from "react";
+import { Button } from "./button";
 
 const Layout = () => {
+  const [isHidden, setIsHidden] = useState(true);
+  const [isDark, setIsDark] = useState("light");
+  const prevScrollY = useRef<number>(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > prevScrollY.current) {
+        setIsHidden(false);
+      } else {
+        setIsHidden(true);
+      }
+      prevScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="">
-      <Navbar />
+      <Navbar
+        className={`fixed z-[9999] transition-transform duration-300 ${
+          isHidden ? "translate-y-0" : "-translate-y-full"
+        }`}
+      />
       <Outlet />
     </div>
   );
