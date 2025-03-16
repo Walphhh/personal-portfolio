d5786bb68204882b6a3017dbc249777da878960c
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-16 19:36:03 +1100

    Added background change fucntionality based on proficiency

--- src/components/ui/TechnologyCard.tsx
+++ src/components/ui/TechnologyCard.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardContent, CardFooter, CardHeader } from "./card";
 import { TechnologyCardProps } from "../information/technologyList";
+import { proficiencyBg } from "../information/technologyList";
 
 const TechnologyCard = (props: TechnologyCardProps) => {
+  const [background, setBackground] = useState<string>();
+
+  useEffect(() => {
+    const background = proficiencyBg.find(
+      (item) => item.key === props.proficiency
+    );
+
+    setBackground(background?.bg);
+  });
+
   return (
-    <Card className="h-30 w-30">
+    <Card className={`h-30 w-30 ${background} `}>
       <div className="inline-block mx-auto">
-        <div>{props.icon ? <props.icon size={40} /> : props.name}</div>
+        <div>
+          {props.icon ? (
+            <props.icon size={40} className="fill-[#242424]" />
+          ) : (
+            props.name
+          )}
+        </div>
       </div>
       <div className="inline-block mx-auto">
-        <h1 className="font-semibold">{props.name}</h1>
+        <h1 className="font-semibold text-[#242424]">{props.name}</h1>
       </div>
     </Card>
   );
