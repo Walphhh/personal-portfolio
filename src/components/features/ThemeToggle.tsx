bdbc011db940b4ed65d071bf9fdd5bbc8c8ae52d
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-16 19:43:44 +1100

    functionality changes to reflect use of themeContext

--- src/components/features/ThemeToggle.tsx
+++ src/components/features/ThemeToggle.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { useTheme } from "../contexts/ThemeContext";
+import { SunIcon, MoonIcon, Sun } from "lucide-react";
 
 const ThemeToggle = () => {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   return (
-    <Button id="toggleTheme" onClick={setTheme}>
-      ToggleMe
+    <Button
+      id="toggleTheme"
+      onClick={setTheme}
+      className="rounded-full p-5 hover:cursor-pointer"
+    >
+      {theme ? <SunIcon /> : <MoonIcon />}
     </Button>
   );
 };
