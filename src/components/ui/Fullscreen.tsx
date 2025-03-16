0dddb40006e7c3ea09cd504d958f5a7a93bc7843
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-16 19:36:22 +1100

    removed id prop

--- src/components/ui/Fullscreen.tsx
+++ src/components/ui/Fullscreen.tsx
@@ -3,10 +3,15 @@ import { ReactNode } from "react";
 interface FullscreenProps {
   children: ReactNode;
   className?: string;
+  id?: string;
 }
 
-const Fullscreen = ({ children, className }: FullscreenProps) => {
-  return <div className={`w-full min-h-dvh ${className}`}>{children}</div>;
+const Fullscreen = ({ children, className, id }: FullscreenProps) => {
+  return (
+    <div id={id} className={`w-full min-h-dvh ${className}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Fullscreen;
