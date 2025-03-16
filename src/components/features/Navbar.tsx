cfde37b0c01940405bdc701ebacdcc871c253978
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-16 19:37:27 +1100

    moved navbar tailwind utils into the  navbar component for close functionality

--- src/components/features/Navbar.tsx
+++ src/components/features/Navbar.tsx
@@ -1,23 +1,106 @@
 import ThemeToggle from "./ThemeToggle";
-interface NavbarProps {
-  className?: string;
-}
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useState, useRef, useEffect } from "react";
 
-const Navbar = ({ className }: NavbarProps) => {
+const Navbar = () => {
+  const [showing, setShowing] = useState(true);
+  const prevScrollY = useRef<number>(0);
+  const navbarRef = useRef<HTMLDivElement | null>(null);
+
+  // Handles Disappear on Scroll Down
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > prevScrollY.current) {
+        setShowing(false);
+      } else {
+        setShowing(true);
+      }
+      prevScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Handles when mouse hovers over the Navbar area
+  // useEffect(() => {
+  //   const navbar = navbarRef.current;
+
+  //   if (navbar) {
+  //     const handleEnter = () => {
+  //       console.log("mouse entered navbar area");
+  //       setShowing(false);
+  //     };
+
+  //     const handleLeave = () => {
+  //       console.log("mouse left navbar area");
+  //       setShowing(true);
+  //     };
+
+  //     navbar.addEventListener("mouseenter", handleEnter);
+  //     navbar.addEventListener("mouseleave", handleLeave);
+
+  //     return () => {
+  //       navbar.removeEventListener("mouseenter", handleEnter);
+  //       navbar.removeEventListener("mouseleave", handleLeave);
+  //     };
+  //   }
+  // });
+
+  // Handles Scrolling to a Section
+  const scrollTo = (sectionID: string) => {
+    const section = document.getElementById(sectionID);
+
+    section?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div
-      className={`w-dvw flex h-16 px-20 bg-optionHighlight items-center ${className}`}
-    >
-      <nav className="w-full">
-        <ul className="flex space-x-24 items-center justify-end color text-fontHighlight">
-          <ThemeToggle />
-          <li>Item One</li>
-          <li>Item Two</li>
-          <li>Item Three</li>
-          <li>Item Four</li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <div
+        ref={navbarRef}
+        className={`w-dvw flex h-16 px-20 bg-optionHighlight items-center fixed z-100 transition-transform duration-300 ${
+          showing ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <nav className="w-full">
+          <ul className="flex space-x-24 items-center justify-end text-l font-semibold text-fontHighlight">
+            <li>
+              <button
+                onClick={() => scrollTo("technologies-section")}
+                className="hover:underline hover:cursor-pointer"
+              >
+                Technologies
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => scrollTo("experience-section")}
+                className="hover:underline hover:cursor-pointer"
+              >
+                Experience
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => scrollTo("projects-section")}
+                className="hover:underline hover:cursor-pointer"
+              >
+                Projects
+              </button>
+            </li>
+            <li>
+              <ThemeToggle />
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 };
 
--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -4,36 +4,9 @@ import { useRef, useState, useEffect } from "react";
 import { Button } from "./button";
 
 const Layout = () => {
-  const [isHidden, setIsHidden] = useState(true);
-  const [isDark, setIsDark] = useState("light");
-  const prevScrollY = useRef<number>(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-
-      if (currentScrollY > prevScrollY.current) {
-        setIsHidden(false);
-      } else {
-        setIsHidden(true);
-      }
-      prevScrollY.current = currentScrollY;
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <div className="">
-      <Navbar
-        className={`fixed z-100 transition-transform duration-300 ${
-          isHidden ? "translate-y-0" : "-translate-y-full"
-        }`}
-      />
+    <div>
+      <Navbar />
       <Outlet />
     </div>
   );
