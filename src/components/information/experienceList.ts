8435411937492b1784c5b092619507670d862ce8
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-16 19:39:11 +1100

    - information changes - migrated proficiencyBg list for more organisation. - added ProficiencyType for type safety

--- src/components/information/experienceList.ts
+++ src/components/information/experienceList.ts
@@ -1,5 +1,3 @@
-import { TagTypes } from "./projectsList";
-
 type experienceType = "work" | "study;";
 
 interface Positions {
@@ -15,7 +13,7 @@ export interface ExperienceCard {
   location: string;
   description: string;
   duration: string;
-  tags: TagTypes[];
+
   positions?: Positions[];
 }
 
@@ -27,9 +25,8 @@ export const experience: ExperienceCard[] = [
     companyLogo: "src/assets/icons/mqu_logo.png",
     location: "Sydney, Australia ",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Handled a diverse range of responsibilities that includes setting up and managing stalls,preparing engagement materials, assisting with calling campaigns, and providing usheringservices during graduations. Leveraged communication skills in order to provide essential information to more than 300 students during stalls regarding current activations and events. Engaged with 80-120 students over the phone per day during calling campaigns to communicate important information and gather data.",
     duration: "2024 - Present",
-    tags: [],
   },
   {
     experienceType: "work",
@@ -38,31 +35,29 @@ export const experience: ExperienceCard[] = [
     companyLogo: "src/assets/icons/mqu_logo.png",
     location: "Sydney, Australia ",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Conducted campus tours for incoming Science and Engineering students around faculty related facilities. Provided wayfinding assistance, ensuring that event attendees were able to navigate the event area effectively. Supported the setup, execution, and packdown of campus events, contributing to a positive student experience",
     duration: "2025 - Present",
-    tags: [],
   },
   {
     experienceType: "work",
     position: "Secretary",
     company: "Filipino Students Society MQ",
-    companyLogo: "src/assets/icons/mqu_logo.png",
+    companyLogo: "src/assets/icons/fssmqu_logo.png",
     location: "Sydney, Australia ",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Enhnced membership experience by streamlining interaction processes with the society. ",
     duration: "2025 - Present",
-    tags: [],
     positions: [
       {
         position: "Director of Events",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          "Directed and executed 4 small scale events with approximately 50 participants each, while managing tight deadlines and multitasking under pressure. Demonstrated strong project management skills through organising logistics, delegating tasks, and ensuring smooth operations during events. Led multiple team meetings and contributed valuable insight that aided in strategic decision-making. Improved team productivity by implementing event planning protocols and processes",
         duration: "2024 - 2025",
       },
       {
         position: "General Officer",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          "Collaborated in organizing and setting up events, contributing to event success and satisfying membership experience. Assisted with tasks including email management, social media sponsorship promotions, and general support.",
         duration: "2023 - 2024",
       },
     ],
